814453fefb32979af062efabca75cdcfa1ebe4ed
attheodo <at@atworks.gr>
2015-11-29 16:05:16 +0200

    Imported user code snippets

---
+++ dispatch_after.m
@@ -0,0 +1,11 @@
+// Dispatch After
+// 
+//
+// IDECodeSnippetCompletionPrefix: dispatchafter
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: A5BF5FD1-F160-491A-959B-77B47FAFB008
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(<#duration#> * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+}
---
+++ mark.m
@@ -0,0 +1,10 @@
+// Mark
+// 
+//
+// IDECodeSnippetCompletionPrefix: mrk
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 6F57EE8D-CAB1-4F83-9FEB-9E4FCE031789
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+// MARK: - <#section#>
